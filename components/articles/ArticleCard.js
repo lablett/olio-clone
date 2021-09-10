b57d223fb5407bd4f15f1608f3c9207ebf530bf9
Lucille Ablett <lucille@60decibels.com>
2021-09-10 15:11:54 +0100

    Scroll to item implemented

--- components/articles/ArticleCard.js
+++ components/articles/ArticleCard.js
@@ -1,10 +1,13 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable camelcase */
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import article from '../../scss/article.module.scss';
 
 
 const ArticleCard = (props) => {
+  const articleRef = React.useRef();
+
   const {
     article: {
       id,
@@ -22,7 +25,7 @@ const ArticleCard = (props) => {
 
   const photoUrl = photos[0].files.medium;
 
-  const className = `${article.card}${isViewed ? `${article.viewed}` : ''}${isSelected ? `${article.expanded}` : ''}`;
+  const className = `${article.card} ${isViewed ? `${article.viewed}` : ''} ${isSelected ? `${article.expanded}` : ''}`;
 
   const expandedContent = (
     <div className={article.expandedContent}>
@@ -32,8 +35,20 @@ const ArticleCard = (props) => {
     </div>
   );
 
+  useEffect(() => {
+    if (isSelected) {
+      articleRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [isSelected]);
+
   return (
-    <div className={className} onClick={() => setSelectedArticle(id)}>
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+    <div
+      ref={articleRef}
+      className={className}
+      onClick={() => setSelectedArticle(id)}
+    >
       <div className={article.columnLeft}>
         <img src={photoUrl} alt="article" />
       </div>
@@ -48,10 +63,10 @@ const ArticleCard = (props) => {
 
 ArticleCard.propTypes = {
   article: PropTypes.shape({
-    id: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
-    photos: PropTypes.object.isRequired,
+    photos: PropTypes.array.isRequired,
     location: PropTypes.object.isRequired,
     collection_notes: PropTypes.string.isRequired,
     expiry: PropTypes.string.isRequired,
